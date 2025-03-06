5095ed399549873b2574a9d4f9611ae38d53adac
Anderson Donaire <andersondonaire@hotmail.com>
2025-03-06 10:25:07 -0300

    Add files via upload

---
+++ BACK-END/API EXPRESS NODE/service/alunos.js
@@ -0,0 +1,13 @@
+import { alunos } from "../data/data.js";
+
+export const listar = (nome)=>{
+    let resultado = alunos;
+    if(nome){
+        resultado = alunos.filter(aluno => aluno.nome.toLowerCase().includes(nome.toLowerCase()));
+    }
+    return resultado;
+}
+
+export const consultarPorId = (_id)=>{
+    return alunos.find(aluno => aluno.id == _id);
+}
